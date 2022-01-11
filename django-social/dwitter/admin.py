b756363ffac7f7a1fdc8f82715049e2549e874ae
edemuner <eduardood15@hotmail.com>
2022-01-11 20:26:52 -0300

    alterado o admin para tornar Profiles um inline de UserAdmin - e dessa maneira tornar a interface mais enxuta

--- django-social/dwitter/admin.py
+++ django-social/dwitter/admin.py
@@ -3,12 +3,16 @@ from django.contrib.auth.models import User, Group
 from .models import Profile
 # Register your models here.
 
+class ProfileInLine(admin.StackedInline):
+    model = Profile
+
 class UserAdmin(admin.ModelAdmin):
     model = User
      # only display the "username" field
     fields = ['username']
+    inlines = [ProfileInLine]
 
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 admin.site.unregister(Group)
-admin.site.register(Profile)
+# admin.site.register(Profile)
