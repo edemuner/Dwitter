4fc69202d36cb4186155c7ade0d6fa77f119705d
edemuner <eduardood15@hotmail.com>
2022-01-11 21:05:36 -0300

    criado um post_save automatizando a criação automática de profile na sequência da criação de um user

--- django-social/dwitter/models.py
+++ django-social/dwitter/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 # Create your models here.
 
@@ -15,3 +16,10 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username
 
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+
+# create a Profile for each new user
+post_save.connect(create_profile, sender=User)
