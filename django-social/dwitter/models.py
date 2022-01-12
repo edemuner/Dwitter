60be3459b8f3be092cbe082138284ca197e71953
edemuner <eduardood15@hotmail.com>
2022-01-11 21:38:13 -0300

    modificada a função create_profile para automaticamente fazer com que o usuário siga a si próprio quando criado

--- django-social/dwitter/models.py
+++ django-social/dwitter/models.py
@@ -20,6 +20,10 @@ def create_profile(sender, instance, created, **kwargs):
     if created:
         user_profile = Profile(user=instance)
         user_profile.save()
+        user_profile.follows.add(instance.profile)
+        # user_profile.follows.set([instance.profile.id])
+        user_profile.save()
+
 
 # create a Profile for each new user
 post_save.connect(create_profile, sender=User)
