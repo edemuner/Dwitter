9a662c9bee12da99efa2135e5045087cf7699d47
edemuner <eduardood15@hotmail.com>
2022-01-13 17:03:02 -0300

    modificado o mecanismo de se obter quem segue o dono do perfil, para se utilizar o atributo related_name em models.Profile

--- django-social/dwitter/views.py
+++ django-social/dwitter/views.py
@@ -13,7 +13,10 @@ def profile_list(request):
 
 def profile(request, pk):
     profile = Profile.objects.get(pk=pk)
-    return render(request, "dwitter/profile.html", {"profile": profile, "followers":get_followers(pk)})
+    return render(request, "dwitter/profile.html", {"profile": profile})
+    # return render(request, "dwitter/profile.html", {"profile": profile, "followers":get_followers(pk)})
 
-def get_followers(pk):
-    return Profile.objects.filter(follows=pk)
+# solução alternativa para obter os seguidores,
+# utilizada antes de verificar a possibilidade de se usar o atributo related_name no model Profile
+# def get_followers(pk):
+#     return Profile.objects.filter(follows=pk)
