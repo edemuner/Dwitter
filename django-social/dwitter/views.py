0160f67ec293ae4dc3f07168161f376cd395f46b
edemuner <eduardood15@hotmail.com>
2022-01-17 20:52:20 -0300

    colocados os decoradores de @login_required encaminhando para a url de login, nas funções onde são necessários, bem como importada a biblioteca necessária

--- django-social/dwitter/views.py
+++ django-social/dwitter/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
 from .models import Profile
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 
-
+@login_required(login_url='/login/')
 def dashboard(request):
     return render(request, "base.html")
 
+@login_required(login_url='/login/')
 def profile_list(request):
     profiles = Profile.objects.exclude(user=request.user)
     return render(request, "dwitter/profile_list.html", {"profiles":profiles})
 
+@login_required(login_url='/login/')
 def profile(request, pk):
     profile = Profile.objects.get(pk=pk)
     if request.method == "POST":
