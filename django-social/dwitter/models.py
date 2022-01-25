5b889d7912f5db1d787b164c9da4025c47bf94f8
edemuner <eduardood15@hotmail.com>
2022-01-25 18:06:27 -0300

    customizada a exibição dos dweets

--- django-social/dwitter/models.py
+++ django-social/dwitter/models.py
@@ -26,6 +26,13 @@ class Dweet(models.Model):
     body = models.CharField(max_length=140)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return(
+            f'{self.user}'
+            f'{self.created_at:%d-%m-%Y %H:%M}'
+            f'{self.body[:30]}...'
+        )
+
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
